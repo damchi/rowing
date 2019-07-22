5ac404018a6df11d9f5257c8e02a3bb389cde53d
Damien Raffi <damien.raffi@me.com>
2019-07-21 22:52:39 -0400

    debut login

--- rowingFront/src/app/app.module.ts
+++ rowingFront/src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {LoginComponent} from "./pages/login/login.component";
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
